ce13b2465473a149049c7da61b93cc9894853e9f
ben Dennett <bendennett@MacBook-Pro-2.local>
2020-05-06 19:48:37 -0600

    some success

--- js/index.js
+++ js/index.js
@@ -40,3 +40,42 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const atags = document.querySelectorAll('a');
+
+atags[0].textContent = "Services";
+atags[1].textContent = "Product";
+atags[2].textContent = "Vision";
+atags[3].textContent = "Features";
+atags[4].textContent = "About";
+atags[5].textContent = "Contact";
+
+const middlehead = document.querySelectorAll('h4');
+
+middlehead[0].textContent = "FEATURES";
+middlehead[1].textContent = "ABOUT";
+middlehead[2].textContent = "SERVICES";
+middlehead[3].textContent = "PRODUCT";
+middlehead[4].textContent = "VISION";
+middlehead[5].textContent = "CONTACT";
+
+const bodytext = document.querySelectorAll('p');
+
+bodytext[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bodytext[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bodytext[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bodytext[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bodytext[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bodytext[5].textContent = "123 Way 456 Street Somewhere, USA";
+bodytext[6].textContent = "1 (888) 888-8888";
+bodytext[7].textContent = "sales@greatidea.io";
+bodytext[8].textContent = "Copyright Great Idea! 2018";
+
+
+let biglogo = document.getElementById("cta-img");
+biglogo.setAttribute('src', "img/header-img.png")
+
+let middlepic = document.getElementById("middle-img");
+middlepic.setAttribute('src', "img/mid-page-accent.jpg")
+
+
